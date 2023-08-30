b01ae34d0548d213366fc53a2caa3183356b1ccf
AnkithaKK <114279424+AnkithaKK@users.noreply.github.com>
2023-08-30 11:25:21 +0530

    Update Assignment2.js

--- Mockfunction for fetch/Assignment2.js
+++ Mockfunction for fetch/Assignment2.js
@@ -1,54 +1,59 @@
 // JavaScript source code
-
+async function fetchapi(url) {
+    try {
+        let response = await mockfetch(url);
+        let posts = await response.json();
+        posts.forEach(post => getposts(post));
+    } catch (err) {
+        return err;
+    }
+}
 async function mockfetch(url) {
     return new Promise(function (resolve, reject) {
         if (url == 'https://jsonplaceholder.typicode.com/posts') {
-            let mockres = {
+            const mockres = {
                 json: () => Promise.resolve([
-                    { id: 1, title: "mockpost-1" },
-                    { id: 2, title: "mockpost-2" },
-                    { id: 3, title: "mockpost-3"}
+                    { id: 1, title: "mockpost-1", body:"quia et suscipcto"},
+                    { id: 2, title: "mockpost-2", body:"est rerum tempore vitae nulla"},
+                    { id: 3, title: "mockpost-3", body:"et iusto  aut"}
                 ])
             };
-            setTimeout(() => resolve(mockres), 2000);
+            setTimeout(() => resolve(mockres));
         }
         else {
             reject(new Error("Can't access"));
         }
     });
 }
-
-async function fetchapi(url) {
-    try {
-        let response = await mockfetch(url);
-        let posts = await response.json();
-        let ul = document.getElementById('postList');
-
-        let i = 1;
-        posts.forEach(post => {
-            let container = document.createElement('div');
-            container.classList.add('posts');
-
-            let title = document.createElement('p');
-            title.append(post.id);
-
-            let body = document.createElement('p');
-            body.append(post.title);
-
-            container.append(`Post ${i} {`);
-
-            container.append(title);
-            container.append(body);
-            container.append('}');
-
-
-            ul.append(container);
-            
-            i++;
-        });
-    } catch (err) {
-        return err;
-    }
+function elem(tag) {
+    return document.createElement(tag);
 }
 
-fetchapi('https://jsonplaceholder.typicode.com/posts').then(null,err=>alert(err));
+function getposts(post) {
+    let ul = document.getElementById('postList');
+    let container = elem('div');
+    let title =elem('p');
+    let body = elem('p');
+    container.classList.add('posts');
+
+    title.textContent = `Title:${post.title}`;
+    body.textContent = `Body:${post.body}`;
+    container.appendChild(title);
+    container.appendChild(body);
+    ul.append(container);
+    document.body.append(ul);  
+}
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.getElementById("loadspinner").style.visibility = "visible";
+    } else {
+        setTimeout(() => {
+            document.getElementById("loadspinner").style.display = "none";
+            document.querySelector("body").style.visibility = "visible";
+        }, 3000)
+    }
+};
+fetchapi('https://jsonplaceholder.typicode.com/posts').then(null);
+
+
